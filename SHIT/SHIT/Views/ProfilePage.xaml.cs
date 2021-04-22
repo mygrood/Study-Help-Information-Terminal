1883b3d1d1c2cad45f78883122f06701933ac628
mygrood <aamikhailovv@gmail.com>
2021-04-22 22:24:40 +0300

    макет профиля и расписания

--- SHIT/SHIT/Views/ProfilePage.xaml.cs
+++ SHIT/SHIT/Views/ProfilePage.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace SHIT.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class TestPage : ContentPage
+    public partial class ProfilePage : ContentPage
     {
-        public TestPage()
+        public ProfilePage()
         {
             InitializeComponent();
         }
---
+++ SHIT/SHIT/Views/TimeTablePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SHIT.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TimeTablePage : ContentPage
+    {
+        public TimeTablePage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            DisplayAlert("тап по дню", "тут должна открыться страничка с днем и подробностями", "ок");
+        }
+
+        
+    }
+}
